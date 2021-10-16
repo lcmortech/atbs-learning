54f99be28397c9c433e9e7b422820c51d1f1a384
Lauren <rg326@live.com>
2021-10-15 22:39:04 -0400

    last files for the day

---
+++ 4-fivetimes.py
@@ -0,0 +1,10 @@
+print('My name is')
+#i = 0
+#while i < 5:
+for i in range(2, 5, 2):
+    print('Jimmy Five Times ' + str(i))
+    #i = i + 1
+#range goes by index, initiating at 0, not 1
+#the amount the variable increases after each iteration
+#negative number decrements
+#you can also use continue and break in for loops
