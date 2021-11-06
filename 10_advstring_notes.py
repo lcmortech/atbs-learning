03a91ab2519f333256165920ed7fb9c3b0ee38db
Lauren <rg326@live.com>
2021-11-05 20:55:03 -0400

    added notes on adv strings

---
+++ 10_advstring_notes.py
@@ -0,0 +1,38 @@
+'Hello'
+'Hello'
+'That is Alice\'s cat'
+"That is Alice's cat"
+#Escape characters
+print('Hello there.\nHow are you?\nI\'m fine.')
+Hello there.
+How are you?
+I'm fine.
+r'Hello'
+'Hello'
+r'That is Carol\'s cat.'
+"That is Carol\\'s cat."
+print(r'That is Carol\'s cat.')
+That is Carol\'s cat.
+'''Multi-line characters
+in Python start and end with triple quotes.'''
+'Multi-line characters\nin Python start and end with triple quotes.'
+print('''Multi-line characters
+in Python start and end with triple quotes.''')
+Multi-line characters
+in Python start and end with triple quotes.
+'Hello world1'
+'Hello world1'
+spam = 'Hello world'
+spam[0]
+'H'
+spam[1:5]
+'ello'
+spam[-1]
+'d'
+'Hello' in spam
+True
+'x' in spam
+False
+'HELLO' in spam
+False
+#The above list operators and methods also work on strings
